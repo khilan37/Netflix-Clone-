52deab5d7d8d063dc20914922e0f453a00aa4818
KHIILAN PATEL <74499010+khilan37@users.noreply.github.com>
2023-08-23 17:15:54 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let btndrawer = document.getElementById("btn-drawer");
+let pdrawer = document.getElementById("pdrawer");
+let btndrawer1 = document.getElementById("btn-drawer1");
+let pdrawer1 = document.getElementById("pdrawer1");
+let btndrawer2 = document.getElementById("btn-drawer2");
+let pdrawer2 = document.getElementById("pdrawer2");
+let btndrawer3 = document.getElementById("btn-drawer3");
+let pdrawer3 = document.getElementById("pdrawer3");
+let btndrawer4 = document.getElementById("btn-drawer4");
+let pdrawer4 = document.getElementById("pdrawer4");
+let btndrawer5 = document.getElementById("btn-drawer5");
+let pdrawer5 = document.getElementById("pdrawer5");
+let icon1 = document.getElementById("icon1");
+let icon2 = document.getElementById("icon2");
+let icon3 = document.getElementById("icon3");
+let icon4 = document.getElementById("icon4");
+let icon5 = document.getElementById("icon5");
+let icon6 = document.getElementById("icon6");
+ 
+btndrawer.addEventListener("click",function(){
+        pdrawer.classList.toggle("p-drawer");
+        icon1.classList.toggle("rotate-icon");
+})
+btndrawer1.addEventListener("click",function(){
+        pdrawer1.classList.toggle("p-drawer");
+        icon2.classList.toggle("rotate-icon");
+
+})
+btndrawer2.addEventListener("click",function(){
+        pdrawer2.classList.toggle("p-drawer");
+        icon3.classList.toggle("rotate-icon");
+
+})
+btndrawer3.addEventListener("click",function(){
+        pdrawer3.classList.toggle("p-drawer");
+        icon4.classList.toggle("rotate-icon");
+
+})
+btndrawer4.addEventListener("click",function(){
+        pdrawer4.classList.toggle("p-drawer");
+        icon5.classList.toggle("rotate-icon");
+
+})
+btndrawer5.addEventListener("click",function(){
+        pdrawer5.classList.toggle("p-drawer");
+        icon6.classList.toggle("rotate-icon");
+
+})
